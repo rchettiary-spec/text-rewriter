208a7130287d11622848f081c920f60ededd4b3d
rchettiary <rchettiary@ebay.com>
2026-03-15 11:53:22 -0700

    clean next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfigimport type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
